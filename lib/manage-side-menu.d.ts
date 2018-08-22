dc3152683902c3c79de8e9c4d1920eaf71a1edea
Aaron Drabeck <github@aarondrabeck.com>
2018-08-22 10:46:54 -0600

    Uses @observe decorator.

--- lib/manage-side-menu.d.ts
+++ lib/manage-side-menu.d.ts
@@ -2,15 +2,14 @@ import '@polymer/iron-selector/iron-selector.js';
 import '@polymer/iron-icon';
 import '@polymer/iron-iconset-svg';
 import { PolymerElement } from '@polymer/polymer';
-export declare class ManageSideMenu extends PolymerElement {
+export declare class ManageSideMenuElement extends PolymerElement {
     page: string;
     devPrefix: string;
     roles: Array<any>;
     isDev: any;
     links: NodeList;
     categories: NodeList;
-    static readonly observers: string[];
-    _isDevChanged(isDev: any): void;
+    _isDevChanged(isDev: boolean): void;
     _onRoleChange(): void;
     static readonly template: HTMLTemplateElement;
 }
